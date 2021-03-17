8e55258ce34cdffe699c70b4c6f52b6a91f70207
KarpovskyiD <karpovskyi.d@gmail.com>
2021-03-17 11:09:18 +0200

    atribute is added

--- lib/Codebreaker/game.rb
+++ lib/Codebreaker/game.rb
@@ -1,6 +1,6 @@
 module Codebreaker
   class Game
-    attr_reader :hints_total, :attempts_total, :hints, :have_hints, :attempts_left, :secret_code, :hints_used
+    attr_reader :hints_total, :attempts_total, :hints, :have_hints, :attempts_left, :secret_code, :hints_used, :round_result
 
     DIFFICULTIES = {
       easy: { attempts: 15, hints: 2 },
