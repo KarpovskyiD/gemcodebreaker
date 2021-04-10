c69ad0bebb7872b79fecf2200add9762329d0afc
KarpovskyiD <karpovskyi.d@gmail.com>
2021-04-10 15:44:58 +0300

    DB class is refactored

--- lib/Codebreaker/models/db.rb
+++ lib/Codebreaker/models/db.rb
@@ -1,22 +1,42 @@
 module Codebreaker
   class DB
-    FOLDER_PATH = './'.freeze
-    DB_NAME = 'statistics'.freeze
+    DB_PATH = './'
+    FILENAME = 'statistics'
+  
     class << self
-      def load_file(filename)
-        YAML.load_file(file_path(filename)) if File.exist?(file_path(filename))
+      def load_file
+        YAML.load_file(file_path) if File.exist?(file_path)
       end
-
-      def file_path(filename)
-        "#{FOLDER_PATH}#{filename}.yml"
+  
+      def file_path
+        "#{DB_PATH}#{FILENAME}.yml"
       end
-
+  
       def save_score(data)
-        current_file_path = file_path(:statistics)
-        File.new(current_file_path, 'w+') unless File.exist?(current_file_path)
-        record = load_file(DB_NAME) || []
-        File.write(current_file_path, (record << data).to_yaml)
+        File.new(file_path, 'w+') unless File.exist?(file_path)
+        record = load_file || []
+        File.write(file_path, (record << data).to_yaml)
+      end
+  
+      def score_data(req, game, player)
+        { name: player.name, difficulty: req.session[:difficulty], attempts_total: game.attempts_total,
+          attempts_used: game.attempts_used, hints_used: game.hints_used }
+      end
+  
+      def sort_st_records(statistics)
+        levels = %w[hard medium easy]
+        statistics.sort_by! do |record|
+          [levels.index(record[:difficulty]), record[:attempts_used], record[:hints_used]]
+        end
+      end
+  
+      def statistics
+        @statistics = load_file
+      end
+  
+      def statistics_set(req)
+        req.session[:statistics] = sort_st_records(statistics)
       end
     end
-  end
+  end  
 end
